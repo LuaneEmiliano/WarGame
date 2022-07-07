0ed8afbeaa5f4dc2c3d54c5e83458fb62999349d
luane <luaneemiliano@gmail.com>
2022-07-07 11:20:31 -0700

    - Add onboarding views and style them. -Add color for create profile border -Check if the user is logged in

--- WarGame/ContentView.swift
+++ WarGame/ContentView.swift
@@ -9,8 +9,54 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Image("background")
+                .ignoresSafeArea()
+            VStack {
+                Spacer()
+                Image("logo")
+                Spacer()
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                Spacer()
+                
+                Image("dealbutton")
+                Spacer()
+                
+                
+                HStack {
+                    Spacer()
+                    VStack {
+                        
+                        Text("Player")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                        Text("0")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                    VStack {
+                        Text("CPU")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                    
+                        Text("0")
+                            .foregroundColor(.white)
+                            .font(.largeTitle)
+                    }
+                    Spacer()
+                }
+              Spacer()
+            }
+        }
     }
 }
 
