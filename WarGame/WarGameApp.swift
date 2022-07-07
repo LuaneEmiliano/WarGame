d41efd4c3b88b7a134b3d181ca7489872f81cf17
luane <luaneemiliano@gmail.com>
2022-07-07 10:48:52 -0700

    Initial Commit

---
+++ WarGame/WarGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameApp.swift
+//  WarGame
+//
+//  Created by luane Niejelski on 7/7/22.
+//
+
+import SwiftUI
+
+@main
+struct WarGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
